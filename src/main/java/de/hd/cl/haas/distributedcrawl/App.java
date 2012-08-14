82b930cf2201a98b988173d2819c4b01753a3689
haas <haas@pool18>
2012-08-14 17:13:05 +0200

    Make App.java starting point for MR job

--- src/main/java/de/hd/cl/haas/distributedcrawl/App.java
+++ src/main/java/de/hd/cl/haas/distributedcrawl/App.java
@@ -1,13 +1,46 @@
 package de.hd.cl.haas.distributedcrawl;
 
+import de.hd.cl.haas.distributedcrawl.map.IndexerMap;
+import de.hd.cl.haas.distributedcrawl.reduce.IndexerReduce;
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Worlddfeqfq!" );
+public class App {
+
+    
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+
+        Job job = new Job(conf, "Indexer");
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        //job.setOutputKeyClass(Text.class);
+        //job.setOutputValueClass(IntWritable.class);
+
+        job.setJarByClass(IndexerMap.class);
+        job.setMapperClass(IndexerMap.class);
+        job.setReducerClass(IndexerReduce.class);
+
+        // TextInputFormat: key is offset in file, value is line
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
     }
 }
