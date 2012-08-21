d21909bb10b92747b7a6f2bc04778beb800e0722
haas <haas@pool18>
2012-08-21 16:48:28 +0200

    Add Skeleton class for IndexMerger Reducer

--- src/main/java/de/hd/cl/haas/distributedcrawl/IndexMerger/MergerReduce.java
+++ src/main/java/de/hd/cl/haas/distributedcrawl/IndexMerger/MergerReduce.java
@@ -4,11 +4,23 @@
  */
 package de.hd.cl.haas.distributedcrawl.IndexMerger;
 
+import de.hd.cl.haas.distributedcrawl.common.Posting;
+import de.hd.cl.haas.distributedcrawl.common.PostingList;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
 /**
  *
- * @author haas
+ * This class (package) merges the output of multiple Indexer (reducer) instances.
+ * 
+ * It also merges output from previous run, thus updating the index.
+ * 
+ * It also sorts a posting list by frequency while eliminating duplicates.
+ * 
+ * @author Michael Haas <haas@cl.uni-heidelberg.de>
  */
 // Input is SequenceFileFormat <Text,TextArrayList>
-public class MergerReduce extends Reducer<>{
+public class MergerReduce extends Reducer<Text,PostingList,Text,Posting>{
+    
     
 }
