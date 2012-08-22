4c54ba5f30c62c4d1d9ed8493cc3621094f6c36e
haas <haas@pool18>
2012-08-22 12:46:25 +0200

    Changes in IndexerMap
    * add crawl delay
    * support new WebDBURL and WebDBURLList
    * refactor code out into methods

--- src/main/java/de/hd/cl/haas/distributedcrawl/Indexer/IndexerMap.java
+++ src/main/java/de/hd/cl/haas/distributedcrawl/Indexer/IndexerMap.java
@@ -4,14 +4,14 @@
  */
 package de.hd.cl.haas.distributedcrawl.Indexer;
 
-import de.hd.cl.haas.distributedcrawl.common.Posting;
-import de.hd.cl.haas.distributedcrawl.common.Term;
-import de.hd.cl.haas.distributedcrawl.common.URLText;
+import de.hd.cl.haas.distributedcrawl.common.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import net.htmlparser.jericho.Element;
 import net.htmlparser.jericho.Source;
 import org.apache.hadoop.fs.FileSystem;
@@ -32,53 +32,59 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
  *
  * @author Michael Haas
  */
-public class IndexerMap extends Mapper<LongWritable, Term, Term, Posting> {
+public class IndexerMap extends Mapper<URLText, WebDBURLList, Term, Posting> {
 
+    /**
+     * How many seconds we wait before hitting a server again.
+     */
+    private static final int CRAWL_DELAY = 3;
+    /**
+     * How many seconds we wait before crawling a website again. This means that
+     * WebDB timestamp for an URL must be older than this value.
+     */
+    private static final int MIN_AGE = 300;
     private SequenceFile.Writer writer;
 
     @Override
-    protected void map(LongWritable key, Term value, Context context) throws IOException, InterruptedException {
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
         if (this.writer == null) {
+            // TODO: update with correct class?
             FileSystem fs = FileSystem.get(context.getConfiguration());
             Path p = new Path(context.getWorkingDirectory(), "foo");
             this.writer = SequenceFile.createWriter(fs, context.getConfiguration(), p, Text.class, Text.class);
-            System.out.println("Writer initialized.");
+            System.err.println("Writer initialized.");
         }
-        System.out.println("key is " + key.toString());
-        System.out.println("value is " + value.toString());
-        URL url = new URL(value.toString());
-        url.openConnection();
-        InputStream stream = url.openStream();
-        Source source = new Source(stream);
-        source.fullSequentialParse();
+    }
 
-        String completeContent = source.getTextExtractor().toString();
-        System.out.println("CompleteContent: ");
-        System.out.println(completeContent);
-        // poor man's tokenizer
-        String[] tokens = completeContent.split(" ");
-
+    private void processLinks(URLText domain, Source source) throws IOException {
         List<Element> anchorElements = source.getAllElements("a");
         for (Element anchorElement : anchorElements) {
             String target = anchorElement.getAttributeValue("href");
-            System.out.println("Anchor target is: " + target);
-            if (target == null)
+            System.err.println("Anchor target is: " + target);
+            if (target == null) {
                 continue;
+            }
+            // is relative domain?
             if (target.startsWith("/")) {
-                target = key + target;
+                target = domain.toString() + target;
             }
             if (!target.startsWith("http")) {
                 System.out.println("URL with unsupported scheme.");
             }
             // we use the URL of the current document as key, but I don't plan
             // on using this in the DB
-            this.writer.append(value, target);
+            // TODO: this is stupid - might make more sense to use the
+            // domain of the link so we can easily sort for that later...
+            this.writer.append(domain, target);
         }
+    }
 
+    private Map<String, Integer> countTokens(String[] tokens) {
         // count absolute frequencies for terms
         HashMap<String, Integer> counts = new HashMap<String, Integer>();
-        for (int ii = 0; ii < tokens.length; ii++) {
-            String term = tokens[ii];
+        for (int jj = 0; jj < tokens.length; jj++) {
+            String term = tokens[jj];
             // clean up string
             term = term.replace(",", "");
             term = term.replace(".", "");
@@ -87,15 +93,48 @@ public class IndexerMap extends Mapper<LongWritable, Term, Term, Posting> {
             }
             counts.put(term, counts.get(term) + 1);
         }
-        Term tTerm = new Term();
-        LongWritable freq = new LongWritable();
-        URLText u = new URLText(value);
-        for (String term : counts.keySet()) {
-            // Emit(term t, posting <n,H{t}>)      
-            freq.set(counts.get(term));
-            Posting p = new Posting(u, freq);
-            tTerm.set(term);
-            context.write(tTerm, p);
+        return counts;
+    }
+
+    @Override
+    protected void map(URLText key, WebDBURLList value, Context context) throws IOException, InterruptedException {
+
+        System.out.println("key is " + key.toString());
+
+        WebDBURL[] urls = value.toArray();
+        for (int ii = 0; ii < urls.length; ii++) {
+            Thread.sleep(CRAWL_DELAY * 1000);
+            WebDBURL dbURL = urls[ii];
+            Date d = dbURL.getDate();
+            Date now = new Date();
+            if ((now.getTime() - d.getTime()) < 300 * 1000) {
+                System.err.println("URL " + dbURL.getText() + " is not old enough, not crawling");
+                continue;
+            }
+
+            URL url = dbURL.getURL();
+            url.openConnection();
+            InputStream stream = url.openStream();
+            Source source = new Source(stream);
+            source.fullSequentialParse();
+            this.processLinks(key, source);
+
+            String completeContent = source.getTextExtractor().toString();
+            System.out.println("CompleteContent: ");
+            System.out.println(completeContent);
+            // poor man's tokenizer
+            String[] tokens = completeContent.split(" ");
+            Map<String, Integer> counts = this.countTokens(tokens);
+
+            Term tTerm = new Term();
+            LongWritable freq = new LongWritable();
+            for (String term : counts.keySet()) {
+                // Emit(term t, posting <n,H{t}>)      
+                freq.set(counts.get(term));
+                Posting p = new Posting(dbURL.getURLText(), freq);
+                tTerm.set(term);
+                context.write(tTerm, p);
+            }
         }
     }
 }
