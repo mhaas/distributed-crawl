2ec418fb0ed8ca98edb5af4d834479716951615a
haas <haas@pool18>
2012-08-22 16:54:18 +0200

    Add Mapper and Reducer for WebDBMerger

---
+++ src/main/java/WebDBMerger/WebDBMergerMapper.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WebDBMerger;
+
+import de.hd.cl.haas.distributedcrawl.common.URLText;
+import de.hd.cl.haas.distributedcrawl.common.WebDBURL;
+import de.hd.cl.haas.distributedcrawl.common.WebDBURLList;
+import java.io.IOException;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ *
+ * This package merges the existing WebDB with the URLs freshly
+ * discovered during the crawl process in @IndexerApp.
+ * @author Michael Haas <haas@cl.uni-heidelberg.de>
+ * 
+ */
+public class WebDBMergerMapper extends Mapper<URLText, WebDBURLList, URLText, WebDBURL> {
+
+    @Override
+    protected void map(URLText key, WebDBURLList value, Context context) throws IOException, InterruptedException {
+       WebDBURL[] urls = value.toArray();
+       for (int ii = 0; ii < urls.length; ii++) {
+           context.write(key, urls[ii]);
+       }
+    }
+    
+}
