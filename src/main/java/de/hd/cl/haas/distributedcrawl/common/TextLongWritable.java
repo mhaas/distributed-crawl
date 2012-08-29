a6784aa2f2675e3c89263e1bc29abb05f6bd8606
haas <haas@pool18>
2012-08-29 11:57:11 +0200

    Make TextLongWritable abstract, fix documentation.

--- src/main/java/de/hd/cl/haas/distributedcrawl/common/TextLongWritable.java
+++ src/main/java/de/hd/cl/haas/distributedcrawl/common/TextLongWritable.java
@@ -1,42 +1,43 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.hd.cl.haas.distributedcrawl.common;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 
 /**
  *
- * Tuple class holding an Text and an Int.
- * 
- * Can be used to hold postings.
- * 
- * 
+ * Abstract tuple class holding an Text and a Long.
+ *
+ * This is sub-classed to implement different key and value classes which need
+ * to hold two values.
+ *
+ *
+ *
  * @author Michael Haas <haas@cl.uni-heidelberg.de>
- * 
+ *
  */
-public class TextLongWritable implements Writable {
+public abstract class TextLongWritable implements Writable {
 
     private Text text = new Text();
     private LongWritable value = new LongWritable();
-    
-    
-    public Text getText(){
+
+    public Text getText() {
         return this.text;
     }
+
     public LongWritable getValue() {
         return this.value;
     }
-    
-    // I assume this would be hidden anyways by ctors with arguments
-    public TextLongWritable() {};
+
+    // Default constructor is needed by the Hadoop framework or
+    // deserialization of objects will fail
+    public TextLongWritable() {
+    }
+
+    ;
     
     /**
      * Constructor.
@@ -49,19 +50,19 @@ public class TextLongWritable implements Writable {
         this.text = t;
         this.value = i;
     }
-    
+
     /**
-     * 
+     *
      * Constructor
-     * 
+     *
      * @param s String instance
-     * @param i Integer instance 
+     * @param i Integer instance
      */
     public TextLongWritable(String s, Integer i) {
         this.text = new Text(s);
         this.value = new LongWritable(i);
     }
-    
+
     @Override
     public void write(DataOutput out) throws IOException {
         this.text.write(out);
@@ -74,7 +75,7 @@ public class TextLongWritable implements Writable {
         // this.text.set(Text.readString(in));
         this.value.readFields(in);
     }
-    
+
     @Override
     public String toString() {
         return this.text.toString() + ";" + this.value.toString();
