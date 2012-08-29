c0e45cbcb1201b5d845ae1758c57ff7ebe158366
haas <haas@pool18>
2012-08-29 11:37:20 +0200

    Update run.sh script

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-IN="dcrawl"
-OUT="dcrawl-out"
 JAR="target/hadoop-deploy/DistributedCrawl-hdeploy.jar"
 #CLASS="de.hd.cl.haas.distributedcrawl.map.IndexerMap"
 CLASS="de.hd.cl.haas.distributedcrawl.App"
-hadoop dfs -rmr "$IN"
-hadoop dfs -copyFromLocal "$IN" . 
-hadoop dfs -rmr "$OUT"
-hadoop jar "$JAR" "$CLASS" "$IN" "$OUT"
-rm -rf "$OUT"
+WEBDB="webdb"
+DIRS="$WEBDB webdb-merged fresh-urls index-raw index-sorted"
+# clean up files to copy
+for token in $DIRS; do
+	hadoop dfs -rmr "$token";
+done
+hadoop dfs -copyFromLocal $WEBDB $WEBDB
+# populate webdb
+hadoop jar "$JAR" "$CLASS" $@
 hadoop dfs -copyToLocal "$OUT" .
+for token in $DIRS; do
+	hadoop dfs -copyToLocal $token .
+done
