7da9d854f66a2e72eb69d8598127307278b67ce1
haas <haas@pool18>
2012-08-29 11:59:59 +0200

    Documentation and whitespace changes.

--- src/main/java/de/hd/cl/haas/distributedcrawl/common/WebDBURLList.java
+++ src/main/java/de/hd/cl/haas/distributedcrawl/common/WebDBURLList.java
@@ -1,11 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.hd.cl.haas.distributedcrawl.common;
 
 import java.util.Collection;
-import java.util.List;
 import org.apache.hadoop.io.ArrayWritable;
 
 /**
