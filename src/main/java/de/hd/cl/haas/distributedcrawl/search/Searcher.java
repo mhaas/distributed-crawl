f95468768ad8a633ca6baa6290394f792d35b698
haas <haas@pool18>
2012-08-29 11:30:56 +0200

    Reset reader position in the index for every term
    If we do not reset the reader, it is possible we are already past the desired term
    and don't return any hits

--- src/main/java/de/hd/cl/haas/distributedcrawl/search/Searcher.java
+++ src/main/java/de/hd/cl/haas/distributedcrawl/search/Searcher.java
@@ -49,14 +49,18 @@ public class Searcher {
                 break;
             }
         }
+        // seek back to beginning of file, otherwise we will miss query results
+        this.reader.close();
+        this.reader = null;
         // prevents obscure NPE in ArrayWritable on empty ArrayWritable
         if (found) {
-        WebDBURL[] converted = l.toArray();
-        HashSet<WebDBURL> s = new HashSet<WebDBURL>(Arrays.asList(converted));
-        return s;
+            WebDBURL[] converted = l.toArray();
+            HashSet<WebDBURL> s = new HashSet<WebDBURL>(Arrays.asList(converted));
+            return s;
         } else {
             return new HashSet<WebDBURL>();
         }
+
     }
 
     /**
@@ -69,11 +73,13 @@ public class Searcher {
 
         HashSet<WebDBURL> res = new HashSet<WebDBURL>();
         if (results.size() > 0) {
+            System.err.println("Adding first result");
             res.addAll(results.get(0));
             results.remove(0);
         }
 
         for (Set<WebDBURL> posting : results) {
+            System.err.println("Adding second to n result..");
             // retainAll is intersection
             if (inclusive) {
                 res.addAll(posting);
@@ -84,25 +90,48 @@ public class Searcher {
         return res;
     }
 
+    private static void printUsage() {
+        System.err.println("First argument: index file name");
+        System.err.println("Second argument: search mode: 'OR', 'AND'");
+        System.err.println("Following arguments: search terms");
+    }
+
     public static void main(String[] args) throws IOException {
-        
+
         Date d1 = new Date();
         Searcher s = new Searcher();
-        s.getReader(args[0]);
-        
+        if (args.length < 3) {
+            printUsage();
+            System.exit(1);
+        }
+        String mode = args[1];
+        boolean inclusive = false;
+        if (mode.equals("OR")) {
+            inclusive = true;
+        } else if (mode.equals("AND")) {
+            inclusive = false;
+        } else {
+            printUsage();
+            System.exit(1);
+        }
+
         List<Set<WebDBURL>> l = new ArrayList<Set<WebDBURL>>();
-        for (int ii = 1; ii < args.length; ii++) {
+        for (int ii = 2; ii < args.length; ii++) {
             String term = args[ii];
             System.err.println("User specified term: " + term);
+            // get new reader for each term
+            s.getReader(args[0]);
             Set<WebDBURL> docs = s.getDocumentsForTerm(term);
+            System.err.println("Got " + docs.size() + " documents for term");
+
             l.add(docs);
         }
-        boolean inclusive = true;
+
         Set<WebDBURL> rs = s.score(l, inclusive);
-        System.err.println("Inclusiveness status: " + inclusive);
+        System.err.println("Merging result sets: " + inclusive);
         System.err.println("Got " + rs.size() + " results");
         for (WebDBURL result : rs) {
-            System.err.println("Got result: " + result);
+            System.err.println("Got result: " + result.getURLText());
         }
         Date d2 = new Date();
         System.err.println("Retrieval took " + (d2.getTime() - d1.getTime()) + "ms");
